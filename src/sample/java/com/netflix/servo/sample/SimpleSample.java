44622dac3cbd834fe5fb14958bcf39d52812d4ce
Greg Orzell <gorzell@gmail.com>
2012-01-06 12:47:44 -0800

    Sample and test files.

---
+++ src/sample/java/com/netflix/servo/sample/SimpleSample.java
@@ -0,0 +1,9 @@
+package com.netflix.servo.sample;
+
+/**
+ * User: gorzell
+ * Date: 1/6/12
+ * Time: 12:45 PM
+ */
+public class SimpleSample {
+}
---
+++ src/test/java/com/netflix/servo/test/TestMonitor.java
@@ -0,0 +1,9 @@
+package com.netflix.servo.test;
+
+/**
+ * User: gorzell
+ * Date: 1/6/12
+ * Time: 12:30 PM
+ */
+public class TestMonitor {
+}
