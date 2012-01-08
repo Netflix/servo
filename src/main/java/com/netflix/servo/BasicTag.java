09a6164c88fa7f2bab78aec0470a17c746269ef9
Greg Orzell <gorzell@gmail.com>
2012-01-07 22:52:42 -0800

    Added files

---
+++ src/main/java/com/netflix/servo/BasicTag.java
@@ -0,0 +1,9 @@
+package com.netflix.servo;
+
+/**
+ * User: gorzell
+ * Date: 1/7/12
+ * Time: 10:12 PM
+ */
+public class BasicTag {
+}
---
+++ src/test/java/com/netflix/servo/BasicTagTest.java
@@ -0,0 +1,42 @@
+package com.netflix.servo;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * User: gorzell
+ * Date: 1/7/12
+ * Time: 10:28 PM
+ */
+public class BasicTagTest {
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+
+    @Test
+    public void testGetKey() throws Exception {
+
+    }
+
+    @Test
+    public void testGetValue() throws Exception {
+
+    }
+
+    @Test
+    public void testParseTag() throws Exception {
+
+    }
+}
