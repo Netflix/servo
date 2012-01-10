794f1a024495a44eb4891323a2cb4b69cb84f79e
Greg Orzell <gorzell@gmail.com>
2012-01-09 18:56:36 -0800

    Tag migration and Unit conversion util.

---
+++ servo-cloudwatch/src/test/java/com/netflix/servo/aws/DataSourceTypeToAwsUnitTest.java
@@ -0,0 +1,15 @@
+package com.netflix.servo.aws;
+
+import org.testng.annotations.Test;
+
+/**
+ * User: gorzell
+ * Date: 1/9/12
+ * Time: 6:44 PM
+ */
+public class DataSourceTypeToAwsUnitTest {
+    @Test
+    public void testGetUnit() throws Exception {
+
+    }
+}
