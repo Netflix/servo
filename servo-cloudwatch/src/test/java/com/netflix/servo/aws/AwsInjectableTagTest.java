0b4243c258aadcc7d21c9beb534e48595f1426c9
Greg Orzell <gorzell@gmail.com>
2012-01-09 21:20:51 -0800

    Added files.

---
+++ servo-cloudwatch/src/test/java/com/netflix/servo/aws/AwsInjectableTagTest.java
@@ -0,0 +1,20 @@
+package com.netflix.servo.aws;
+
+import org.testng.annotations.Test;
+
+/**
+ * User: gorzell
+ * Date: 1/9/12
+ * Time: 9:11 PM
+ */
+public class AwsInjectableTagTest {
+    @Test
+    public void testGetKey() throws Exception {
+
+    }
+
+    @Test
+    public void testGetValue() throws Exception {
+
+    }
+}
