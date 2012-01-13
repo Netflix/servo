3f5a624646fd170c06404deb0e040ddb4195051c
Greg Orzell <gorzell@gmail.com>
2012-01-12 21:54:45 -0800

    Add tag comparator.

---
+++ servo-core/src/main/java/com/netflix/servo/TagComparator.java
@@ -0,0 +1,9 @@
+package com.netflix.servo;
+
+/**
+ * User: gorzell
+ * Date: 1/12/12
+ * Time: 9:49 PM
+ */
+public class TagComparator {
+}
---
+++ servo-core/src/test/java/com/netflix/servo/TagComparatorTest.java
@@ -0,0 +1,15 @@
+package com.netflix.servo;
+
+import org.testng.annotations.Test;
+
+/**
+ * User: gorzell
+ * Date: 1/12/12
+ * Time: 9:52 PM
+ */
+public class TagComparatorTest {
+    @Test
+    public void testCompare() throws Exception {
+
+    }
+}
