7a6e7c095a6009465dd6bcdef8f23dbf61ff9c44
Brian Harrington <brharrington@gmail.com>
2012-01-25 07:25:20 -0800

    enum for the standard tag keys

---
+++ servo-core/src/main/java/com/netflix/servo/StandardTagKeys.java
@@ -0,0 +1,41 @@
+/*
+ * #%L
+ * servo
+ * %%
+ * Copyright (C) 2011 Netflix
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.netflix.servo;
+
+/**
+ * Standard tag keys that are used within this library.
+ */
+public enum StandardTagKeys {
+    /** Canonical name for the class that is providing the metric. */
+    CLASS_NAME("ClassName"),
+
+    /** Hostname for the system reporting the metric. */
+    HOST("Host");
+
+    private String keyName;
+
+    private StandardTagKeys(String keyName) {
+        this.keyName = keyName;
+    }
+
+    public String getKeyName() {
+        return keyName;
+    }
+}
