e5fff192f74e86c55577bbca49142c6bd44b1855
Greg Orzell <gorzell@gmail.com>
2012-04-18 11:41:47 -0700

    Issue #43 Added tags to the JMX object name, re-enabled unit test.

--- servo-core/src/test/java/com/netflix/servo/jmx/JmxMonitorRegistryTest.java
+++ servo-core/src/test/java/com/netflix/servo/jmx/JmxMonitorRegistryTest.java
@@ -56,7 +56,7 @@ public class JmxMonitorRegistryTest {
         registry.unRegisterObject(null);
     }
 
-    //@Test
+    @Test
     //TODO Until tags are included in the ObjectName this will not work.
     public void testUnRegisterObject() throws Exception {
         MonitorRegistry registry = newInstance();
