9ddd4bb5e4f3b09ec1aa8317912b82dd93dcceed
Brian Harrington <brharrington@gmail.com>
2012-07-03 10:53:07 -0700

    add javadoc comments

--- servo-core/src/main/java/com/netflix/servo/monitor/AnnotatedNumberMonitor.java
+++ servo-core/src/main/java/com/netflix/servo/monitor/AnnotatedNumberMonitor.java
@@ -27,7 +27,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 /**
- * Wraps an annotated field and exposes it as a monitor object.
+ * Wraps an annotated field and exposes it as a numeric monitor object.
  */
 class AnnotatedNumberMonitor extends AbstractMonitor<Number> implements NumericMonitor<Number> {
 
