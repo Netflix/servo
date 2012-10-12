9cbebf0d30d1b3dbde497e36513db75e18dcf373
Daniel Muino <dmuino@netflix.com>
2012-10-12 16:19:24 -0700

    Add javadocs

--- servo-core/src/main/java/com/netflix/servo/monitor/DefaultPublishingPolicy.java
+++ servo-core/src/main/java/com/netflix/servo/monitor/DefaultPublishingPolicy.java
@@ -1,5 +1,9 @@
 package com.netflix.servo.monitor;
 
+/**
+ * The default publishing policy. Observers must follow the default behaviour when the {@link MonitorConfig}
+ * associated with a {@link Monitor} uses this policy.
+ */
 public class DefaultPublishingPolicy implements PublishingPolicy {
     private static DefaultPublishingPolicy INSTANCE = new DefaultPublishingPolicy();
     private DefaultPublishingPolicy() {}
--- servo-core/src/main/java/com/netflix/servo/monitor/PublishingPolicy.java
+++ servo-core/src/main/java/com/netflix/servo/monitor/PublishingPolicy.java
@@ -1,4 +1,7 @@
 package com.netflix.servo.monitor;
 
+/**
+ * A Publishing Policy allows us to customize the behavior of different observers.
+ */
 public interface PublishingPolicy {
 }
