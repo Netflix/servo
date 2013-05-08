162958670c607d04cbf5d4d659a92ad2ffae53af
Greg Orzell <gorzell@gmail.com>
2013-05-07 20:59:26 -0700

    Review fixes.

--- servo-core/src/main/java/com/netflix/servo/monitor/TimedStopwatch.java
+++ servo-core/src/main/java/com/netflix/servo/monitor/TimedStopwatch.java
@@ -1,24 +1,36 @@
+/**
+ * Copyright 2013 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.netflix.servo.monitor;
 
+import com.google.common.base.Preconditions;
+
 /**
- * Date: 5/3/13
- * Time: 11:36 AM
- *
- * @author gorzell
- *         Stopwatch that will also record to a timer.
+ * Stopwatch that will also record to a timer.
  */
 public class TimedStopwatch extends BasicStopwatch {
     private final Timer timer;
 
     public TimedStopwatch(Timer timer) {
+        Preconditions.checkNotNull(timer);
         this.timer = timer;
     }
 
     @Override
     public void stop() {
         super.stop();
-        if (timer != null) {
-            timer.record(getDuration(timer.getTimeUnit()));
-        }
+        timer.record(getDuration(timer.getTimeUnit()));
     }
 }
