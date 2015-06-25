ee37eadc84b038e18384525239b1dbf7f216b16b
Daniel Muino <dmuino@netflix.com>
2015-06-25 16:32:46 -0700

    Replace regex replaceAll with hand crafted version

--- servo-atlas/src/main/java/com/netflix/servo/publish/atlas/ValidCharacters.java
+++ servo-atlas/src/main/java/com/netflix/servo/publish/atlas/ValidCharacters.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 Netflix, Inc.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,50 +21,75 @@ import com.netflix.servo.tag.Tag;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 /**
  * Utility class to deal with rewriting keys/values to the character set accepted by atlas.
  */
 public final class ValidCharacters {
-  /**
-   * Only allow letters, numbers, underscores, dashes and dots in our identifiers.
-   */
-  private static final Pattern INVALID_CHARS = Pattern.compile("[^a-zA-Z0-9_\\-\\.]");
+    /**
+     * Only allow letters, numbers, underscores, dashes and dots in our identifiers.
+     */
 
-  private ValidCharacters() {
-    // utility class
-  }
+    private static boolean[] charIsAllowed = new boolean[128];
 
-  /**
-   * Convert a given string to one where all characters are valid.
-   */
-  public static String toValidCharset(String str) {
-    return INVALID_CHARS.matcher(str).replaceAll("_");
-  }
-
-  /**
-   * Return a new metric where the name and all tags are using the valid character
-   * set.
-   */
-  public static Metric toValidValue(Metric metric) {
-    MonitorConfig cfg = metric.getConfig();
-    MonitorConfig.Builder cfgBuilder = MonitorConfig.builder(toValidCharset(cfg.getName()));
-    for (Tag orig : cfg.getTags()) {
-      cfgBuilder.withTag(toValidCharset(orig.getKey()), toValidCharset(orig.getValue()));
+    static {
+        charIsAllowed['.'] = true;
+        charIsAllowed['-'] = true;
+        charIsAllowed['_'] = true;
+        for (char ch = '0'; ch <= '9'; ch++) {
+            charIsAllowed[ch] = true;
+        }
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            charIsAllowed[ch] = true;
+        }
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            charIsAllowed[ch] = true;
+        }
     }
-    cfgBuilder.withPublishingPolicy(cfg.getPublishingPolicy());
-    return new Metric(cfgBuilder.build(), metric.getTimestamp(), metric.getValue());
-  }
 
-  /**
-   * Create a new list of metrics where all metrics are using the valid character set.
-   */
-  public static List<Metric> toValidValues(List<Metric> metrics) {
-    List<Metric> fixedMetrics = new ArrayList<Metric>(metrics.size());
-    for (Metric m : metrics) {
-      fixedMetrics.add(toValidValue(m));
+    private ValidCharacters() {
+        // utility class
     }
-    return fixedMetrics;
-  }
+
+    /**
+     * Convert a given string to one where all characters are valid.
+     */
+    public static String toValidCharset(String str) {
+        final int n = str.length();
+        final StringBuilder buf = new StringBuilder(n + 1);
+        for (int i = 0; i < n; i++) {
+            final char c = str.charAt(i);
+            if (c < charIsAllowed.length && charIsAllowed[c]) {
+                buf.append(c);
+            } else {
+                buf.append('_');
+            }
+        }
+        return buf.toString();
+    }
+
+    /**
+     * Return a new metric where the name and all tags are using the valid character
+     * set.
+     */
+    public static Metric toValidValue(Metric metric) {
+        MonitorConfig cfg = metric.getConfig();
+        MonitorConfig.Builder cfgBuilder = MonitorConfig.builder(toValidCharset(cfg.getName()));
+        for (Tag orig : cfg.getTags()) {
+            cfgBuilder.withTag(toValidCharset(orig.getKey()), toValidCharset(orig.getValue()));
+        }
+        cfgBuilder.withPublishingPolicy(cfg.getPublishingPolicy());
+        return new Metric(cfgBuilder.build(), metric.getTimestamp(), metric.getValue());
+    }
+
+    /**
+     * Create a new list of metrics where all metrics are using the valid character set.
+     */
+    public static List<Metric> toValidValues(List<Metric> metrics) {
+        List<Metric> fixedMetrics = new ArrayList<Metric>(metrics.size());
+        for (Metric m : metrics) {
+            fixedMetrics.add(toValidValue(m));
+        }
+        return fixedMetrics;
+    }
 }
---
+++ servo-atlas/src/test/java/com/netflix/servo/publish/atlas/ValidCharactersTest.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2015 Netflix, Inc.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.servo.publish.atlas;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class ValidCharactersTest {
+    @Test
+    public void testValidStrIsUnchanged() throws Exception {
+        String valid = "abc09.-_";
+        assertEquals(ValidCharacters.toValidCharset(valid), valid);
+    }
+
+    @Test
+    public void testInvalidStrIsFixed() throws Exception {
+        String str = "abc09.-_ abc";
+        assertEquals(ValidCharacters.toValidCharset(str), "abc09.-__abc");
+
+        String boundaries = "\u0000\u0128\uffff";
+        assertEquals(ValidCharacters.toValidCharset(boundaries), "___");
+    }
+}
