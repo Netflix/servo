2edddb715a9346fc483d42290483f274cc10ceb5
Daniel Muino <dmuino@netflix.com>
2015-07-24 10:03:07 -0700

    Indent with 4 spaces

--- servo-atlas/src/main/java/com/netflix/servo/publish/atlas/AtlasMetric.java
+++ servo-atlas/src/main/java/com/netflix/servo/publish/atlas/AtlasMetric.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 Netflix, Inc.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,66 +28,65 @@ import java.io.IOException;
  * A metric that can be reported to Atlas.
  */
 class AtlasMetric implements JsonPayload {
+    private final MonitorConfig config;
+    private final long start;
+    private final double value;
 
-  private final MonitorConfig config;
-  private final long start;
-  private final double value;
-
-  AtlasMetric(Metric m) {
-    this(m.getConfig(), m.getTimestamp(), m.getNumberValue());
-  }
-
-  AtlasMetric(MonitorConfig config, long start, Number value) {
-    this.config = Preconditions.checkNotNull(config, "config");
-    this.value = Preconditions.checkNotNull(value, "value").doubleValue();
-    this.start = start;
-  }
-
-  MonitorConfig getConfig() {
-    return config;
-  }
-
-  long getStartTime() {
-    return start;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (!(obj instanceof AtlasMetric)) {
-      return false;
+    AtlasMetric(Metric m) {
+        this(m.getConfig(), m.getTimestamp(), m.getNumberValue());
     }
-    AtlasMetric m = (AtlasMetric) obj;
-    return config.equals(m.getConfig())
-        && start == m.getStartTime()
-        && Double.compare(value, m.value) == 0;
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(config, start, value);
-  }
-
-  @Override
-  public String toString() {
-    return "AtlasMetric{config=" + config
-        + ", start=" + start + ", value=" + value + '}';
-  }
-
-  @Override
-  public void toJson(JsonGenerator gen) throws IOException {
-    gen.writeStartObject();
-
-    gen.writeObjectFieldStart("tags");
-    gen.writeStringField("name", config.getName());
-    for (Tag tag : config.getTags()) {
-      gen.writeStringField(tag.getKey(), tag.getValue());
+    AtlasMetric(MonitorConfig config, long start, Number value) {
+        this.config = Preconditions.checkNotNull(config, "config");
+        this.value = Preconditions.checkNotNull(value, "value").doubleValue();
+        this.start = start;
     }
-    gen.writeEndObject();
 
-    gen.writeNumberField("start", start);
-    gen.writeNumberField("value", value);
+    MonitorConfig getConfig() {
+        return config;
+    }
 
-    gen.writeEndObject();
-    gen.flush();
-  }
+    long getStartTime() {
+        return start;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof AtlasMetric)) {
+            return false;
+        }
+        AtlasMetric m = (AtlasMetric) obj;
+        return config.equals(m.getConfig())
+                && start == m.getStartTime()
+                && Double.compare(value, m.value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(config, start, value);
+    }
+
+    @Override
+    public String toString() {
+        return "AtlasMetric{config=" + config
+                + ", start=" + start + ", value=" + value + '}';
+    }
+
+    @Override
+    public void toJson(JsonGenerator gen) throws IOException {
+        gen.writeStartObject();
+
+        gen.writeObjectFieldStart("tags");
+        gen.writeStringField("name", config.getName());
+        for (Tag tag : config.getTags()) {
+            gen.writeStringField(tag.getKey(), tag.getValue());
+        }
+        gen.writeEndObject();
+
+        gen.writeNumberField("start", start);
+        gen.writeNumberField("value", value);
+
+        gen.writeEndObject();
+        gen.flush();
+    }
 }
--- servo-atlas/src/main/java/com/netflix/servo/publish/atlas/BasicAtlasConfig.java
+++ servo-atlas/src/main/java/com/netflix/servo/publish/atlas/BasicAtlasConfig.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 Netflix, Inc.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,26 +20,26 @@ package com.netflix.servo.publish.atlas;
  * values.
  */
 public class BasicAtlasConfig implements ServoAtlasConfig {
-  @Override
-  public String getAtlasUri() {
-    return System.getProperty("servo.atlas.uri");
-  }
+    @Override
+    public String getAtlasUri() {
+        return System.getProperty("servo.atlas.uri");
+    }
 
-  @Override
-  public int getPushQueueSize() {
-    String pushQueueSize = System.getProperty("servo.atlas.queueSize", "1000");
-    return Integer.parseInt(pushQueueSize);
-  }
+    @Override
+    public int getPushQueueSize() {
+        String pushQueueSize = System.getProperty("servo.atlas.queueSize", "1000");
+        return Integer.parseInt(pushQueueSize);
+    }
 
-  @Override
-  public boolean shouldSendMetrics() {
-    String enabled = System.getProperty("servo.atlas.enabled", "true");
-    return Boolean.parseBoolean(enabled);
-  }
+    @Override
+    public boolean shouldSendMetrics() {
+        String enabled = System.getProperty("servo.atlas.enabled", "true");
+        return Boolean.parseBoolean(enabled);
+    }
 
-  @Override
-  public int batchSize() {
-    String batch = System.getProperty("servo.atlas.batchSize", "10000");
-    return Integer.parseInt(batch);
-  }
+    @Override
+    public int batchSize() {
+        String batch = System.getProperty("servo.atlas.batchSize", "10000");
+        return Integer.parseInt(batch);
+    }
 }
